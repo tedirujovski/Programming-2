77f2a80b8154f9850d4e7b423a3fcba1eb9b59fc
tedirujovski <36259754+tedirujovski@users.noreply.github.com>
2018-03-08 11:37:55 +0200

    Map

--- main.cpp
+++ main.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
-#include <list>;
+#include <map>
 
 using namespace std;
-void nums()
+void arr()
 {
     int sum=0;
-    list <int>number;
-    number.push_front(3);
-    number.push_front(1);
-    number.push_back(5);
-    number.push_back(7);
-    list<int>::iterator it;
-    it = number.begin();
-    for(int i=0;i<=3;i++)
+    map<string,int>map1;
+    map1["Bulgaria"]=7000000;
+    map1["China"]=1000000000;
+    map1["Russia"]=500000000;
+    map1["Mexico"]=50000000;
+    map1["Germany"]=50000000;
+    map<string,int>::iterator it;
+    it=map1.begin();
+    while(it!=map1.end())
     {
-        cout<<*it<<" ";
-        sum=sum+*it;
+        cout<<it->first<<"-"<<it->second<<endl;
+        sum=sum+it->second;
         it++;
     }
     cout<<"sum="<<sum;
 }
+void arr2()
+{
+    string song,singer;
+    map<string,string>map2;
+    cin>>song;
+    cin>>singer;
+    map2[song]=singer;
+    cin>>song;
+    cin>>singer;
+    map2[song]=singer;
+    cin>>song;
+    cin>>singer;
+    map2[song]=singer;
+    cin>>song;
+    cin>>singer;
+    map2[song]=singer;
+    cin>>song;
+    cin>>singer;
+    map2[song]=singer;
+    map<string,string>::iterator iter;
+    iter=map2.begin();
+    while(iter!=map2.end())
+    {
+        cout<<iter->first<<"-"<<iter->second<<endl;
+        iter++;
+    }
+}
 
 int main()
 {
-    nums();
+    arr();
+    cout<<endl<<endl;
+    arr2();
 }
